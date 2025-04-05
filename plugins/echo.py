f3f6bea6cb10471412069dd61416ed377e76f7e9
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-04-05 19:33:57 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -30,7 +30,7 @@ from plugins.database.database import db
 from plugins.database.add import AddUser
 from pyrogram.types import Thumbnail
 
-
+cookies = "cookies.txt"
 
 
 @Client.on_message(filters.private & filters.regex(pattern=".*http.*"))
